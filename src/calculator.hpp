a8536f4c2d4768856a23ee3a1926add5da30385e
Sviat Naum <suatoslau@gmail.com>
2023-10-23 12:13:41 +0300

    alhpa test

--- src/calculator.hpp
+++ src/calculator.hpp
@@ -9,8 +9,8 @@ public:
     template<class T, class  U>
     auto add(T a, U b)
     {
-        if(std::isnan(a) && std::isnan(b))
-            std::cout<<a+b<<"\n";
+        if(!std::isnan(a) && !std::isnan(b))
+            std::cout<<a+b<<std::endl;
         else
             std::cout<<"you'r numbers is not NaN\n";
     }
@@ -18,8 +18,8 @@ public:
     template<class U, class K>
     auto substract(U a, K b)
     {
-         if(std::isnan(a) && std::isnan(b))
-            std::cout<<a-b<<"\n";
+         if(!std::isnan(a) && !std::isnan(b))
+            std::cout<<a-b<<std::endl;
          else
             std::cout<<"you'r numbers is not NaN\n";
     }
@@ -27,8 +27,8 @@ public:
     template<class K, class G>
     auto multiply(K a, G b)
     {
-        if(std::isnan(a) && std::isnan(b))
-            std::cout<<a*b<<"\n"; 
+        if(!std::isnan(a) && !std::isnan(b))
+            std::cout<<a*b<<std::endl; 
         else
             std::cout<<"you'r numbers is not NaN\n";  
     }
@@ -36,8 +36,13 @@ public:
     template<class L,class A>
     auto divide(L a, A b)
     {
-        if(std::isnan(a) && std::isnan(b))
-            std::cout<<a/b<<"\n";
+        if(!std::isnan(a) && !std::isnan(b))
+            {
+                if(a == 0 || b == 0)
+                    std::cout<<"ERROR :  u want divide by 0\n";
+                else
+                    std::cout<<a/b<<std::endl;
+            }
         else
             std::cout<<"you'r numbers is not NaN\n";
     }
@@ -46,9 +51,11 @@ public:
 class Interface
 {
 public :
-    static void hello();
-    static void help();
-    static void clear_scr();
-    static void info();
+    static void hello(){std::cout<<"Hello in our calculator app !)\n\tFor now, you can only manipulate numbers with 2 in functions : add, subtract, divide, multiply.\n"
+    <<"To add 2 numbers of type a, for substract of type s and other..."<<"pls, choose what you want to do?\n";}
+    /*static void help(){ std::cout<<"For now, you can only manipulate numbers with 2 in functions : add, subtract, divide, multiply.\n"
+    <<"To add 2 numbers of type a, for substract of type s and other...";}*/
+    static void clear_scr(){system("cls");}
+    static void info(){ std::cout<<"------------------ This app created by Sviatoslav Naum. All rights reserved ------------------\n";}
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,25 @@
 #include "calculator.hpp"
 
-void Interface::hello()
-{
-    std::cout<<"Hello in our calculator app !)\n"<<"pls, choose what you want to do?\n";
-}
-void Interface::help()
-{   
-    std::cout<<"For now, you can only manipulate numbers with 2 in functions : add, subtract, divide, multiply.\n"
-    <<"To add 2 numbers of type a, for substract of type s and other...";
-}
-void Interface::info()
-{
-    std::cout<<"------------------ This app created by Sviatoslav Naum. All rights reserved ------------------\n";
-}
-void Interface::clear_scr()
-{
-    system("cls");
-}
-
 int main()
 {
     Interface interface;
     interface.hello();
-    char type{};
+    char type{};   
     std::cin>>type;
+
+    Logic logic;
+    auto a{0.0},b{0.0};
+    std::cin>>a>>b;
+
     switch(type)
     {
         case 'c' : interface.clear_scr(); break;
-        case 'h' : interface.help();break;
+        //case 'h' : interface.help();break;
         case 'i' : interface.info();break;
+        case 'a' : logic.add(a,b);system("pause");break;
+        case 'd' : logic.divide(a,b);system("pause");break;
+        case 'm' : logic.multiply(a,b);system("pause");break;
+        case 's' : logic.substract(a,b); system("pause");break;
         default : return -1;
     }
     return 0;
